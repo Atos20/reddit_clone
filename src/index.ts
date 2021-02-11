ba256541888c54ad2c47b6d6a34f84afb4b7758d
Orlando Murcio <ato.murcio@gmail.com>
2021-02-10 21:38:18 -0700

    Set up mikro config file

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
-import { MikroORM } from "@mikro-orm/core"
-import { __prod__ } from "./constants"
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+// import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+import express from "express";
 
 const start = async() => {
-  const orm = await MikroORM.init({
-    entities: [],
-    dbName: 'reddit',
-    type: 'postgresql',
-    debug: !__prod__,
-  })
-}
+  console.log("dirname:", __dirname);
+  const orm = await MikroORM.init(microConfig)
+  await orm.getMigrator().up()
+  
+  const app = express();
+  const port = 5000
+  app.get('/', (_, res) => {
+    res.send('hello please second place plase hell yeah')
+  });
 
-start()
+  app.listen(port, () => {
+    console.log(`server started on lacalhost:${port}`)
+  });
+};
 
-console.log('hello world again')
+start().catch(error => {
+  console.log(error)
+})
+
